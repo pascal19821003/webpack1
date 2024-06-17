a34e8bad49bf440dba8b66661cbaaadacce24190
pascal19821003 <pascal1982@sina.com>
2024-06-17 20:12:13 +0800

    add configuration

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-// import Header from './header/Header'
+import Header from "header/src/Header";
+import Bottom from "bottom/src/Bottom";
 
 export default function Hello() {
   return (
     <div>
       Hello, World!
-      {/* <Header></Header> */}
+      <Header></Header>
+      <Bottom></Bottom>
     </div>
   );
 }
